8497b59328605dc40d682b2c8c480a6140bfdb04
gwanghun-choi <choi_huni@naver.com>
2025-12-03 14:44:38 +0900

    init: service-a

---
+++ src/main/java/com/example/servicea/controller/StatusController.java
@@ -0,0 +1,21 @@
+package com.example.servicea.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class StatusController {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @Value("${service-b.url}")
+    private String serviceBUrl;
+
+    @GetMapping("/a/status")
+    public String status() {
+        String bStatus = restTemplate.getForObject(serviceBUrl + "/b/status", String.class);
+        return "A OK | B: " + bStatus;
+    }
+}
