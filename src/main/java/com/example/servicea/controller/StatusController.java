4ebf44f4b5f32ef126a985edd5544e544208f989
gwanghun-choi <choi_huni@naver.com>
2025-12-16 14:22:33 +0900

    fix: change url

--- src/main/java/com/example/servicea/controller/StatusController.java
+++ src/main/java/com/example/servicea/controller/StatusController.java
@@ -1,13 +1,19 @@
 package com.example.servicea.controller;
 
+import com.example.servicea.mapper.TestMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+
 @RestController
+@RequiredArgsConstructor
 public class StatusController {
 
+    private final TestMapper testMapper;
     private final RestTemplate restTemplate = new RestTemplate();
 
     @Value("${service-b.url}")
@@ -22,5 +28,10 @@ public class StatusController {
     @GetMapping("/a/health")
     public String health() {
         return "200";
+    }
+
+    @GetMapping("/a/test")
+    public Map<String, Object> testDb() {
+        return testMapper.selectDbInfo();
     }
 }
